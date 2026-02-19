6d0921b079c55f4c6236225f265cf388e8fa8405
Robert Setter <robert.setter@bobertdos.me>
2026-02-19 16:36:33 -0600

    +length output and quantity specification

--- src/LuhnConsole/Program.cs
+++ src/LuhnConsole/Program.cs
@@ -1,19 +1,25 @@
 ﻿using LuhnNet;
 
 string luhn, state;
+int n = 10;
 state = "is";
 if (args.Length == 1 && args[0].ToLower().Equals("-c"))
 {
     Console.Write("Enter a number to validate: ");
     luhn = Console.ReadLine();
     state = Luhn.IsValid(luhn) ? "is" : "is not";
-    Console.WriteLine($"{luhn} {state} a valid Luhn number!");
+    Console.WriteLine($"{luhn} is {luhn.Length} characters long.\r\nIt {state} a valid Luhn number!");
+}
+else if (args.Length == 1 && int.TryParse(args[0], out n))
+{
+    Console.WriteLine($"{n} test numbers requested!");
 }
 else
 {
-    for (int i = 1; i <= 10; i++)
-    {
-        Console.WriteLine(Luhn.Generate());
-    }
+    Console.WriteLine($"Here are {n} test numbers!");
+}
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine(Luhn.Generate());
 }
 Console.ReadKey();
